5088d498f157e346644d2158c745d58aa2b37d2a
rastov.va <vrastov@gmail.com>
2016-02-16 21:28:55 +0300

    #2 Добавленна простейшая формочка для добавления кандидатов, кандидаты добовляются в массив на клиенте.

--- client/src/module/app.js
+++ client/src/module/app.js
@@ -19,5 +19,14 @@
                 surname:'Васичкин'
             }
         ];
+
+        $scope.add = function(){
+            if (angular.isDefined($scope.name) && angular.isDefined($scope.surname)){
+                $scope.candidates.push({
+                    name:$scope.name,
+                    surname:$scope.surname
+                });
+            }
+        }
     });
 })(angular);
