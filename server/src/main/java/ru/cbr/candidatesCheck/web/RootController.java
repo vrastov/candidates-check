c25f654bf27ed5359676dae292dbfe666db681e7
rastov.va <vrastov@gmail.com>
2016-02-16 21:04:20 +0300

    #1 Переписал контролер RootController.java чтоб он выдавал при запросе файл index.html из проекта клиента. В зависимости добавлена библиотека commons-io.

--- server/src/main/java/ru/cbr/candidatesCheck/web/RootController.java
+++ server/src/main/java/ru/cbr/candidatesCheck/web/RootController.java
@@ -1,15 +1,26 @@
 package ru.cbr.candidatesCheck.web;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+
+
 @Controller
 public class RootController {
     @RequestMapping(value = "/",method = RequestMethod.GET)
     @ResponseBody
-    String index(){
-        return "Hello World!!!";
+    void root(HttpServletResponse response) throws IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        File index = new File("client/src/index.html");
+        String data = FileUtils.readFileToString(index, "UTF-8");
+        IOUtils.write(data, response.getWriter());
     }
 }
